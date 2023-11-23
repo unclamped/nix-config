54758889c31d275ea47db323ebc3bd1e62f63861
unclamped <clear6860@tutanota.com>
2023-11-23 20:31:52 -0300

    feat: initial configuration
    Signed-off-by: unclamped <clear6860@tutanota.com>

---
+++ disko-config.nix
@@ -0,0 +1,76 @@
+#{ hostName, target-disk, ... }
+{
+  disko.devices = {
+    disk = {
+      vda = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "vda-opened";
+                passwordFile = "/tmp/secret.key"; # Interactive
+                settings.allowDiscards = true;
+                extraFormatArgs = [
+                  "--cipher serpent-xts-plain64"
+                  "--key-size 512"
+                  "--iter-time 10000"
+                  #"--hash whirlpool"
+                  "--label vda-crypt"
+                ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "--label vda" ];
+                  subvolumes = {
+                    "/fsroot" = {
+                      mountpoint = "/";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                    "/persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                    "/swap" = {
+                      mountpoint = "/.swapvol";
+                      swap.swapfile.size = "20M";
+                    };
+                  };
+                  #postCreateHook = ''
+                  #    MNTDIR=$(mktemp -d)
+                  #    mount -t btrfs /dev/mapper/vda-opened $MNTDIR
+                  #    # trap 'umount $MNTDIR; rm -rf $MNTDIR' EXIT
+                  #    btrfs subvolume snapshot -r $MNTDIR/fsroot $MNTDIR/fsroot-blank
+                  #'';
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
