22a3b80364cc73b4d67afcd689fef9c56bc4ff72
unclamped <clear6860@tutanota.com>
2023-11-23 22:51:13 -0300

    h

--- flake.nix
+++ flake.nix
@@ -3,20 +3,34 @@
       
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    home-manager.url = "github:nix-community/home-manager";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows =
+        "nixpkgs"; # Use system packages list where available
+    };
+
+    nur.url = "github:nix-community/nur";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, nur }: {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          (import home-manager.nixosModules.home-manager)
+          home-manager.nixosModules.home-manager
           {
             home-manager.extraSpecialArgs = { inherit unstable; };
           }
+          nur.nixosModules.nur
         ];
       };
     };
