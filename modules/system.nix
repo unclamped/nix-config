e30b9c5cb94f66d4dda6166e5a3ddfdd18813e70
unclamped <clear6860@tutanota.com>
2023-11-24 18:40:32 -0300

    h

--- modules/system.nix
+++ modules/system.nix
@@ -62,7 +62,7 @@
   '';
 
   fonts = {
-    fonts = with pkgs; [
+    packages = with pkgs; [
       # icon fonts
       material-design-icons
 
@@ -76,7 +76,7 @@
     ];
 
     # use fonts specified by user rather than default ones
-    enableDefaultFonts = false;
+    enableDefaultPackages = false;
 
     # user defined fonts
     # the reason there's Noto Color Emoji everywhere is to override DejaVu's
