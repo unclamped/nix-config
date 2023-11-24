0565b14f121801c60ace0f1a6e94320141f4b203
unclamped <clear6860@tutanota.com>
2023-11-23 22:55:49 -0300

    a

--- flake.nix
+++ flake.nix
@@ -30,7 +30,6 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit unstable; };
           }
           nur.nixosModules.nur
         ];
