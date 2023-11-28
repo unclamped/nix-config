45adc7051ecbee07bec4a6ac92dcac73d6a1d046
unclamped <clear6860@tutanota.com>
2023-11-28 00:19:59 -0300

    nvm

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -17,7 +17,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Define your hostname.
-  networking.hostName = "${user.username}";
+  networking.hostName = "${system.hostName}";
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
