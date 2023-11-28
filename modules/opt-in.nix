8279d7466ff698dab6400c956ae659417011dbe4
unclamped <clear6860@tutanota.com>
2023-11-27 23:19:32 -0300

    Implement impermanence

--- modules/opt-in.nix
+++ modules/opt-in.nix
@@ -1,4 +1,8 @@
+{ config, ... }:
+
 {
+  imports = [ ./impermanence/nixos.nix ];
+
   # grahamc's "Erase your darlings" implemented for Btrfs. Thanks accelbread and oposs for the help
   boot.initrd.postResumeCommands = ''
     mkdir -vp /tmp
@@ -23,7 +27,37 @@
 
   users.mutableUsers = false;
 
+  # FIXME: For some reason this doesn't work!
   security.sudo.extraConfig = "Defaults lecture=\"never\"";
 
-  # TODO: Impermanence
+  environment.persistence."/persistent" = {
+    hideMounts = true;
+    directories = [
+      "/var/log"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+    ];
+    files = [
+      "/etc/machine-id"
+      { file = "/etc/nix/id_rsa"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+    ];
+    users."${user.username}" = {
+      directories = [
+        "Downloads"
+        "Music"
+        "Pictures"
+        "Documents"
+        "Videos"
+        { directory = ".gnupg"; mode = "0700"; }
+        { directory = ".ssh"; mode = "0700"; }
+        { directory = ".nixops"; mode = "0700"; }
+        { directory = ".local/share/keyrings"; mode = "0700"; }
+        ".local/share/direnv"
+      ];
+      files = [
+        ".screenrc"
+      ];
+    };
+  };
 }
